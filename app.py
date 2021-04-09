00ff8e4ddcb8b52530ed69ca51a944a632783f13
TimeSenz <1003823662@qq.com>
2021-04-09 13:28:05 +0800

    '添加后台模拟实现'

--- app.py
+++ app.py
@@ -1,17 +1,61 @@
-from flask import Flask
+import os
+
+from flask import Flask, url_for
 from flask import render_template
+import json
+import pymysql
+import getData
+
 
 app = Flask(__name__)
 
 
+
+def db_conn():
+    conn = pymysql.connect(host="127.0.0.1", user="root", password="bettertree@123", database="lol", charset="utf8")
+    # 得到一个可以执行SQL语句的光标对象
+    cursor = conn.cursor()
+
+    sql = """
+		CREATE TABLE USER1 (
+		id INT auto_increment PRIMARY KEY ,
+		name CHAR(10) NOT NULL UNIQUE,
+		age TINYINT NOT NULL
+		)ENGINE=innodb DEFAULT CHARSET=utf8;
+		"""
+    # 执行SQL语句
+    cursor.execute(sql)
+
+    # 关闭光标对象
+    cursor.close()
+
+    # 关闭数据库连接
+    conn.close()
+
 @app.route('/')
 def hello_world():
-    return render_template('index.html');
-
+    banner = getData.banner()
+    competition = getData.competition()
+    notice = getData.notice()
+    activity = getData.activity()
+    link = getData.link()
+    item={
+        "banner": banner,
+        "competition": competition,
+        "notice": notice,
+        "activity": activity,
+        "link": link
+    }
+    return render_template('index.html', items=item)
 
 @app.route('/login')
 def login():
-    return render_template('login/login.html');
+    return render_template('login/login.html')
+
+@app.route('/create')
+def create():
+    db_conn()
+    return "create message"
 
 
 if __name__ == '__main__':
---
+++ getData.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, url_for
+from flask import render_template
+import json
+
+
+app = Flask(__name__)
+
+def banner():
+    filename = os.path.join(app.static_folder, 'data/banner.json')
+    with open(filename, encoding="utf-8") as f:
+        banner = json.load(f);
+    return banner;
+
+def competition():
+    filename = os.path.join(app.static_folder, 'data/competition.json')
+    with open(filename, encoding="utf-8") as f:
+        competition = json.load(f);
+    return competition;
+
+def notice():
+    filename = os.path.join(app.static_folder, 'data/notice.json')
+    with open(filename, encoding="utf-8") as f:
+        notice = json.load(f);
+    return notice;
+
+def activity():
+    filename = os.path.join(app.static_folder, 'data/activity.json')
+    with open(filename, encoding="utf-8") as f:
+        activity = json.load(f);
+    return activity;
+
+def link():
+    filename = os.path.join(app.static_folder, 'data/link.json')
+    with open(filename, encoding="utf-8") as f:
+        link = json.load(f);
+    return link;
